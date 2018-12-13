46874045c4bad35d6e34efb8dd349d7cbfe70dc8
18645956947 <18645956947@163.com>
2018-12-13 10:56:07 +0800

    letcode-数组重复问题： 用HashSet判断数组是否重复

---
+++ LetCode/src/zhx/day1213/Solution.java
@@ -0,0 +1,28 @@
+package zhx.day1213;
+
+import java.util.HashSet;
+/**
+ * 用HashSet判断数组是否重复
+ * @author zhouhaixin
+ *
+ */
+public class Solution {
+	public boolean containsDuplicate(int[] nums) {
+		HashSet<Integer> h1 = new HashSet<Integer>();
+		for(int i = 0;i<nums.length;i++) {
+			if(h1.contains(nums[i])) {
+				return true;
+			}
+			else {
+				h1.add(nums[i]);
+			}
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		Solution s1 = new Solution();
+		int[] num = new int[] {1,1,1,3,3,4,3,2,4,2};//true
+		s1.containsDuplicate(num);
+	}
+
+}
