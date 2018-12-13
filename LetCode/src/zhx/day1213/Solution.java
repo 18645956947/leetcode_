6c2bb12c909fada3d9e2ba78635844d893172f7c
18645956947 <18645956947@163.com>
2018-12-13 11:23:52 +0800

    无

--- LetCode/src/zhx/day1213/Solution.java
+++ LetCode/src/zhx/day1213/Solution.java
@@ -5,7 +5,7 @@ import java.util.HashSet;
  * 用HashSet判断数组是否重复
  * @author zhouhaixin
  *@version1.0
- *dsfghshffghfgfhethgbsdrth
+ *
  */
 public class Solution {
 	public boolean containsDuplicate(int[] nums) {
