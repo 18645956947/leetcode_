5b986eea785657eb7ab0d548c7219617eccc0294
18645956947 <18645956947@163.com>
2018-12-13 11:07:09 +0800

    123

---
+++ LetCode/src/dddd.java
@@ -0,0 +1,19 @@
+
+public class dddd {
+	public static void main(String[] args) {
+		int []num1 = new int[4];
+		int []num2 = new int[4];
+		num1[0] = 1;
+		for(int j = 0;j<num1.length;j++){
+			System.out.print(num1[j]);
+	    }
+		
+		System.out.println();
+		System.out.print("----------------");
+		System.out.println();
+		
+		for(int j = 0;j<num2.length;j++){
+			System.out.print(num2[j]);
+	    }
+	}
+}
