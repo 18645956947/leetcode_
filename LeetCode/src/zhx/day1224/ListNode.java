206896dfbe3f66bf72fa32b8bc19e564a00af4e8
18645956947 <18645956947@163.com>
2019-01-07 11:50:21 +0800

    add some functions

--- LeetCode/src/zhx/day1224/ListNode.java
+++ LeetCode/src/zhx/day1224/ListNode.java
@@ -1,8 +1,26 @@
 package zhx.day1224;
 
+
 public class ListNode {
-	int data;
-	ListNode next;
-	ListNode(int data) { this.data = data; }
+	public int val;
+	public ListNode next;
+	public ListNode(int data) { this.val = data; }
+	public void printListNode(ListNode head) {
+		if(head==null) {
+			return;
+		}
+		ListNode cur = head;
+		System.out.println("原链表数据为：");
+		System.out.println("---------");
+		while(cur != null) {
+			System.out.print(cur.val);
+			cur = cur.next;
+		}
+		System.out.println();
+		System.out.println("---------");
+	}
+	/*
+	 * @Override public String toString() { return String.valueOf(this.val); }
+	 */
 
 }
