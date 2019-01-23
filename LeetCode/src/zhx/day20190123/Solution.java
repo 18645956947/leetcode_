00e0023f4a2a4a5659c01d448d16f9ed37f4d1be
18645956947 <18645956947@163.com>
2019-01-23 10:10:11 +0800

    add description

--- LeetCode/src/zhx/day20190123/Solution.java
+++ LeetCode/src/zhx/day20190123/Solution.java
@@ -9,6 +9,10 @@ import zhx.day20190108.TreeNode;
 * @author lenovo
 * @date 2019年1月23日上午9:48:59 
 * @Description: 检查对称二叉树
+* 首先了解对称(镜像)二叉树 是什么意思，其实就是左边放一棵树，中间放在一个镜子，
+* 你去想像，在镜子里的树会是什么样子，假设根节点是root 那么root.left == 镜子里的 root.right
+* root.left.left == 镜子里的 root.right.right  root.left.right == 镜子里的 root.right.left
+* 然后就按照自己的逻辑去写代码咯
 */
 public class Solution {
 	public boolean isSymmetric(TreeNode root) {
