fa483822fecf82030717ae19f32f29ce7041e22e
18645956947 <18645956947@163.com>
2019-03-04 19:21:54 +0800

    add sth

--- LeetCode/src/DeleteDuplicates.java
+++ LeetCode/src/DeleteDuplicates.java
@@ -1,10 +1,40 @@
+;
 
 /** 
 * @author lenovo
 * @date 2019年3月4日下午7:18:35 
-* @Description: 
+* @Description: 82. 删除排序链表中的重复元素 II
+* 采用双指针来删除链表
 */
 public class DeleteDuplicates {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	public ListNode deleteDuplicates(ListNode head) {
+    if(head == null || head.next == null){
+        return head;
+    }
+    ListNode h = new ListNode(-1);
+    h.next = head;
+    ListNode pre = h;
+    ListNode cur = head;
+    while(cur != null){
+        boolean flag = true;
+        while(cur.next != null && cur.val == cur.next.val){
+            cur = cur.next;
+            flag = false;
+        }
+        if(flag){ 
+            pre = cur;
+        } else{
+            pre.next = cur.next;
+        }
+        cur = cur.next;
+    }
+    return h.next;
+}
 
 }
  
