c65f7db2ef87b90b853ec86ab052790729f791ec
18645956947 <18645956947@163.com>
2019-03-04 19:35:18 +0800

    链表

--- LeetCode/src/zhx/day20181225/LinkList.java
+++ LeetCode/src/zhx/day20181225/LinkList.java
@@ -37,22 +37,20 @@ public class LinkList {
 
         current = node;
         while (current != null) {
-            System.out.println(current.data);
+            System.out.print(current.data);
             current = current.next;
         }
     }
 
-
     class Node {
         //注：此处的两个成员变量权限不能为private，因为private的权限是仅对本类访问。
-        int data; //数据域
-        Node next;//指针域
+        public int data; //数据域
+        public Node next;//指针域
 
         public Node(int data) {
             this.data = data;
         }
     }
-
 
     public static void main(String[] args) {
         LinkList list = new LinkList();
