3d4eef605b19f207cac06227a3194b068f135fe8
18645956947 <18645956947@163.com>
2019-03-04 19:24:33 +0800

    add sth

--- LeetCode/src/zhx/day20190304/DeleteDuplicates.java
+++ LeetCode/src/zhx/day20190304/DeleteDuplicates.java
@@ -3,9 +3,15 @@ package zhx.day20190304;
 /** 
 * @author lenovo
 * @date 2019年3月4日下午4:27:02 
-* @Description: 
+* @Description: 83. 删除排序链表中的重复元素
+* 有序链表的去重（前一个与后一个相互比较）
 */
 public class DeleteDuplicates {
+	class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
 	public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null){
             return head;
@@ -20,7 +26,6 @@ public class DeleteDuplicates {
             }
         }
         return head;
-        
     }
 }
  
