bbc367ac7043bd62c6bf6f1497150a6d99464908
18645956947 <18645956947@163.com>
2019-03-05 10:31:40 +0800

    add sth

--- LeetCode/src/zhx/day20190305/MiddleNode.java
+++ LeetCode/src/zhx/day20190305/MiddleNode.java
@@ -6,7 +6,7 @@ import zhx.day1224.ListNode;
 * @author lenovo
 * @date 2019年3月5日上午10:21:28 
 * @Description: 876. 链表的中间结点
-* 
+* 思路是先遍历一遍 获得链表长度的，然后再找到中间结果，下次思考怎么能只是遍历一遍
 */
 public class MiddleNode {
 	public ListNode middleNode(ListNode head) {
