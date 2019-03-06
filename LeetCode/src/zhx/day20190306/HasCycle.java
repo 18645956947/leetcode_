a658d43e62b5de0448baade80180a34bb59e1122
18645956947 <18645956947@163.com>
2019-03-06 09:50:54 +0800

    fix bug

--- LeetCode/src/zhx/day20190306/HasCycle.java
+++ LeetCode/src/zhx/day20190306/HasCycle.java
@@ -1,11 +1,51 @@
-package zhx.day20190306; 
+package zhx.day20190306;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import zhx.day1224.ListNode;
 
 /** 
 * @author lenovo
 * @date 2019年3月6日上午9:40:07 
-* @Description: 
+* @Description: 141. 环形链表
 */
 public class HasCycle {
-
+	//解法一：采用快慢指针
+	public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow  = head;
+        while(head == null|| head.next == null){
+            return false;
+        }
+        while(fast.next != null&&fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(fast == slow){
+                return true;
+            }            
+        }
+        return false;
+    }
+	
+	//解法二  采用set集合 时间的复杂度O(n),空间复杂度O(n)
+	public boolean hasCycle_01(ListNode head) {
+		Set<ListNode> set = new HashSet<>();
+		ListNode node = head;
+		if(head == null || head.next == null) {
+			return false;
+		}
+		while(node != null) {
+			if(set.contains(node)) {
+				return true;
+			}
+			else {
+				set.add(node);
+			}
+			node = node.next;
+		}
+		return false;
+	}
 }
  
