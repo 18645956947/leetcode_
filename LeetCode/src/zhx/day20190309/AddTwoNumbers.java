35493aae5bc2256544db9b6ff70d4da55b144a77
18645956947 <18645956947@163.com>
2019-03-09 21:06:32 +0800

    2. 两数相加

---
+++ LeetCode/src/zhx/day20190309/AddTwoNumbers.java
@@ -0,0 +1,31 @@
+package zhx.day20190309;
+
+import zhx.day1224.ListNode;
+
+/** 
+* @author lenovo
+* @date 2019年3月9日下午9:05:51 
+* @Description: 
+*/
+public class AddTwoNumbers {
+	public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+	    ListNode dummyHead = new ListNode(0);
+	    ListNode p = l1, q = l2, curr = dummyHead;
+	    int carry = 0;
+	    while (p != null || q != null) {
+	        int x = (p != null) ? p.val : 0;
+	        int y = (q != null) ? q.val : 0;
+	        int sum = carry + x + y;
+	        carry = sum / 10;
+	        curr.next = new ListNode(sum % 10);
+	        curr = curr.next;
+	        if (p != null) p = p.next;
+	        if (q != null) q = q.next;
+	    }
+	    if (carry > 0) {
+	        curr.next = new ListNode(carry);
+	    }
+	    return dummyHead.next;
+	}
+}
+ 
