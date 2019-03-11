3b3c99341994fb2b995ae27481fc57be434da462
18645956947 <18645956947@163.com>
2019-03-11 19:02:38 +0800

    add Note

--- LeetCode/src/List/RemoveElements.java
+++ LeetCode/src/List/RemoveElements.java
@@ -1,12 +1,19 @@
-package List; 
+package List;
+
+import javax.naming.LimitExceededException;
+import javax.security.auth.callback.LanguageCallback;
 
 /** 
 * @author lenovo
 * @date 2019年3月11日下午6:37:44 
-* @Description: 
+* @Description: 203. 移除链表元素
 */
+/**
+ * 思路就是先创建一个节点指向头节点，挨个遍历 ...
+ */
 public class RemoveElements {
-	public ListNode removeElements(ListNode head, int val) {
+	//思路就是先创建一个节点指向头节点，挨个遍历 ...
+	public  static ListNode removeElements(ListNode head, int val) {
         ListNode newNode = new ListNode(-1);
         ListNode p = newNode;
         newNode.next = head;
@@ -20,5 +27,15 @@ public class RemoveElements {
         return newNode.next;
         
     }
+	public static void main(String[] args) {
+		ListNode node = new ListNode(1);
+		node.next = new ListNode(2);
+		node.next.next = new ListNode(6);
+		RemoveElements.removeElements(node, 6);
+		while(node != null) {
+			System.out.println(node.val);
+			node = node.next;
+		}
+	}
 }
  
